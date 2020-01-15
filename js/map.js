a2e05bb0429a5d2715d495259d91f13071d98469
Lai Yongquan <lai_yongquan@tech.gov.sg>
2020-01-15 16:43:18 +0800

    first commit. gonna use npm to manage the dependencies. i wonder why did i start this way.

---
+++ js/map.js
@@ -0,0 +1,64 @@
+// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+function setupMap() {
+
+    var center = L.bounds([1.56073, 104.11475], [1.16, 103.502]).getCenter();
+    var map = L.map('mapdiv').setView([center.x, center.y], 12);
+
+    
+    // var basemap = onemapTiles()
+    var basemap = osmTiles()
+
+    map.setMaxBounds([[1.56073, 104.1147], [1.16, 103.502]]);
+
+    basemap.addTo(map);
+
+    return map
+
+}
+
+
+function onemapTiles(){
+    var basemap = 
+    L.tileLayer('https://maps-{s}.onemap.sg/v3/Original/{z}/{x}/{y}.png', {
+        detectRetina: true,
+        maxZoom: 18,
+        minZoom: 11,
+        //Do not remove this attribution
+        attribution: '<img src="https://docs.onemap.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> New OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
+    });
+    return basemap
+}
+
+function osmTiles(){
+    var basemap =
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        detectRetina: true,
+        maxZoom: 19,
+        minZoom: 11,
+        //Do not remove this attribution
+        attribution: '<img src="https://docs.onemap.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> New OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
+    });
+    return basemap
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+
+function showPosition(position) {
+    marker = new L.Marker([position.coords.latitude, position.coords.longitude], { bounceOnAdd: false }).addTo(map);
+    var popup = L.popup()
+        .setLatLng([position.coords.latitude, position.coords.longitude])
+        .setContent('You are here!')
+        .openOn(map);
+}
+
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     accessToken: 'your.mapbox.access.token'
+// }).addTo(mymap);
